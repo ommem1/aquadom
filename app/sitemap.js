b4f960afe65324c699b007fb2e81ff7c02c04c41
ommem1 <yura69592@gmail.com>
2026-05-25 23:13:07 +0500

    Add files via upload

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://aquadom.uz',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
